8f9860841aef6f6f57ba9066ba33271e8cf86642
= <=>
2024-02-06 15:41:24 -0700

    added initial message popup

---
+++ src/components/MessagePopup.js
@@ -0,0 +1,9 @@
+// Displays a message
+export default function MessagePopup(props) {
+  return (
+    <div className='message-popup'>
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  )
+}
