6c29d8258699b8cfc4a837b3fac31dcf1e653dba
= <=>
2024-02-13 15:38:41 -0700

    connected next block to state, set colors properly

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -1,13 +1,16 @@
+import { useSelector } from 'react-redux';
+
+import { shapes } from '../utils'
 import GridSquare from './GridSquare';
 
 // Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
-
-  const block = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+  const nextShape = useSelector((state) => state.nextShape);
+  const block = shapes[nextShape][0];
   // Map the block to the grid
   const grid = block.map((rowArray, row) => {
     return rowArray.map((square, col) => {
-      return <GridSquare key={`${row}${col}`} color={square} />
+      return <GridSquare key={`${row}${col}`} color={square === 0 ? 0 : nextShape} />
     })
   })
 
