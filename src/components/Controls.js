16e69e5a5200dcbefc6fe158ba9f8bed5ee56e23
= <=>
2024-02-13 17:48:40 -0700

    add rotate reducer

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
 export default function Controls(props) {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
   const { isRunning } = useSelector((state) => state)
 
   return (
